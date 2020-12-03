9e9c049da4d33950055799992ad6bf8c9a00e7e4
Franco Ortega <francopdx@gmail.com>
2020-12-03 14:08:22 -0800

    add eslintrc and scripts

---
+++ data.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+  };
+
+
+  function getFirstInArray(arr) {
+    const first = arr[0];
+    return first;
+  };
+
+
+
+  function getSecondInArray(arr)) {
+    const second = arr[1];
+    return second;
+  };
+
+  function getSecondInArray(arr) {
+    const second = arr[1;
+    return second;
+  };
