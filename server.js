4c0bcaefb18ee97e7169c35cf90f0f7a3f8bfff0
Franco Ortega <francopdx@gmail.com>
2020-12-03 14:36:52 -0800

    add initial server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    console.log(req.body);
+    res.send('Hi there!');
+})
+
+app.listen(1234, () => {
+    console.log('started on PORT 1234');
+})
