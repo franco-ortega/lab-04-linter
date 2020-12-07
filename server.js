5be85800e7e73b9a92d774b4cfc16d50c10a0efb
Franco Ortega <francopdx@gmail.com>
2020-12-06 19:47:55 -0800

    clean up code in server.js

--- server.js
+++ server.js
@@ -6,24 +6,12 @@ const { linter } = require('./linter');
 app.use(express.json());
 
 app.post('/api/v1/lint', (req, res) =>{
-    console.log('This is the START of the post ENDPOINT');
 
-    // The item that will be posted needs to be checked for errors (I think???), but that happens later
-    const testObject = {
-        array: [1, 2, 3],
-        speed: 'fast',
-        parenthesis: '()'
-    };
-    console.log('Below this line is the testObject.');
-
-    // The item needs to be put through the functions to transform it into just the necessary characters
     const stringyItem = turnItemIntoString(req.body.code);
     const allCharacters = placeStringIntoArray(stringyItem)
     const remaingingCharacters = stripAwayExcessCharacters(allCharacters);
     const finalStack = linter(remaingingCharacters);
     const stackResult = checkStack(finalStack);
-
-    console.log('This is the END of the post ENDPOINT');
 
     res.send(stackResult);
 })
