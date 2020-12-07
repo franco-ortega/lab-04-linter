ead62e5975d4d5fcebf07ee23e460f3ce6de6c3f
Franco Ortega <francopdx@gmail.com>
2020-12-06 19:44:21 -0800

    clean up code in stack.js

--- stack.js
+++ stack.js
@@ -8,28 +8,17 @@ class Stack {
     // put item on top of stack
     push(item) {
         this.stack.push(item)
-        console.log('This item was pushed: ' + item);
     }
     
     // take item off the top of stack and return that item
     pop() {
         const poppedItem = this.stack.pop();
-        console.log('This item was popped: ' + poppedItem);
         return poppedItem;
     }
   
     // look at item on top of stack
     peek() {
-        console.log(this.stack);
         const [ lastItem ] = this.stack.slice(-1);
-        console.log('LAST ITEM:')
-        console.log(lastItem)
-
-        // const lastItemIndex = this.stack.length;
-        // console.log(lastItemIndex);
-
-        // const lastItemNumber = parseInt(lastItemIndex);
-        // console.log(lastItemNumber); 
     }
   }
 
